20fa9cfe959602093514cdc00e480a782f084317
kristinus <kristinus@3651135e-374b-0410-bedc-b208083bf7df>
2009-04-21 14:31:41 +0000

    Version 4.2.0

---
+++ redaxo/tags/4.2.0/redaxo/include/addons/tinymce/uninstall.inc.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * TinyMCE Addon
+ *
+ * @author markus[dot]staab[at]redaxo[dot]de Markus Staab
+ *
+ * @author andreas[dot]eberhard[at]redaxo[dot]de Andreas Eberhard
+ * @author <a href="http://rex.andreaseberhard.de">rex.andreaseberhad.de</a>
+ *
+ * @author Dave Holloway
+ * @author <a href="http://www.GN2-Netwerk.de">www.GN2-Netwerk.de</a>
+ *
+ * @package redaxo4
+ * @version svn:$Id$
+ */
+
+	$REX['ADDON']['install']['tinymce'] = 0;
+?>
